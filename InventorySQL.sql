bdb7b02f342003236faea961878fa02c1b8f8853
Ilya Yarovikov <yarovikov.ilha@yandex.ru>
2024-10-24 16:03:24 +0300

    Commit 1: Create DataBases

---
+++ InventorySQL.sql
@@ -0,0 +1,33 @@
+--
+-- Файл сгенерирован с помощью SQLiteStudio v3.4.4 в Чт окт 24 15:57:37 2024
+--
+-- Использованная кодировка текста: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Таблица: House
+CREATE TABLE IF NOT EXISTS House (HouseID INTEGER NOT NULL PRIMARY KEY, Name TEXT (255) NOT NULL, Location TEXT (255) NOT NULL);
+INSERT INTO House (HouseID, Name, Location) VALUES (228, 'WR', 'Карла Маркса 98');
+INSERT INTO House (HouseID, Name, Location) VALUES (666, 'Warp', 'Московская 32');
+INSERT INTO House (HouseID, Name, Location) VALUES (1337, 'Pudge', 'Ярославская 1');
+
+-- Таблица: Operations
+CREATE TABLE IF NOT EXISTS Operations (OperationID INTEGER PRIMARY KEY NOT NULL, productID INTEGER REFERENCES Products (ProductID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL, houseID INTEGER REFERENCES House (HouseID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL, TypeOperation TEXT NOT NULL, Kol TEXT (255) NOT NULL);
+INSERT INTO Operations (OperationID, productID, houseID, TypeOperation, Kol) VALUES (21, 11, 666, 'Расход', '25');
+INSERT INTO Operations (OperationID, productID, houseID, TypeOperation, Kol) VALUES (22, 16, 1337, 'Приход', '2');
+INSERT INTO Operations (OperationID, productID, houseID, TypeOperation, Kol) VALUES (23, 17, 228, 'Приход', '5');
+INSERT INTO Operations (OperationID, productID, houseID, TypeOperation, Kol) VALUES (24, 13, 666, 'Расход', '50');
+
+-- Таблица: Products
+CREATE TABLE IF NOT EXISTS Products (ProductID INTEGER PRIMARY KEY NOT NULL, ProductName TEXT (255) NOT NULL, UnitPrice REAL NOT NULL, Category TEXT (100) NOT NULL);
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (11, 'Молоко', 70.0, 'Продукты');
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (12, 'Хлеб', 36.0, 'Продукты');
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (13, 'Доширак', 89.0, 'Продукты');
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (14, 'Пицца', 189.0, 'Выпечка');
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (15, 'Беляш', 45.0, 'Выпечка');
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (16, 'АК-74М', 5000.0, 'Игрушки');
+INSERT INTO Products (ProductID, ProductName, UnitPrice, Category) VALUES (17, 'Mauzer', 2500.0, 'Игрушки');
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
