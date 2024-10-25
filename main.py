b817a35c1e55af63150e4788d9186499a13a4f93
Koczy <alek6677889900@gmail.com>
2024-10-25 14:25:33 +0300

    Interface correction

--- main.py
+++ main.py
@@ -69,65 +69,48 @@ class InventoryApp:
         self.operation = Operation(self.db)
 
         self.root.title("Inventory Management")
-
-        # Установка элементов интерфейса
         self.setup_ui()
-
-        # Показать список продуктов при запуске
         self.show_products()
 
     def setup_ui(self):
-        # Увеличиваем шрифт
         button_font = ("Arial", 14)
-
-        # Таблица для продуктов, складов и операций
         self.tree = ttk.Treeview(self.root, show="headings")
         self.tree.grid(row=0, column=0, columnspan=5, padx=20, pady=20, sticky="nsew")
-
-        # Кнопка для добавления продукта
-        self.button_add_product = tk.Button(self.root, text="Add Product", font=button_font,
+        self.button_add_product = tk.Button(self.root, text="Добавить товар", font=button_font,
                                             command=self.add_product_window)
         self.button_add_product.grid(row=1, column=0, padx=10, pady=10)
-
-        # Кнопка для удаления продукта
-        self.button_delete_product = tk.Button(self.root, text="Delete Product", font=button_font,
+        self.button_delete_product = tk.Button(self.root, text="Удалить товары", font=button_font,
                                                command=self.delete_product)
         self.button_delete_product.grid(row=1, column=1, padx=10, pady=10)
-
-        # Кнопка для отображения товаров
-        self.button_show_products = tk.Button(self.root, text="Show Products", font=button_font,
+        self.button_show_products = tk.Button(self.root, text="Показать товары", font=button_font,
                                               command=self.show_products)
         self.button_show_products.grid(row=1, column=2, padx=10, pady=10)
-
-        # Кнопка для отображения складов
-        self.button_show_houses = tk.Button(self.root, text="Show Houses", font=button_font, command=self.show_houses)
+        self.button_show_houses = tk.Button(self.root, text="Показать склады", font=button_font, command=self.show_houses)
         self.button_show_houses.grid(row=1, column=3, padx=10, pady=10)
-
-        # Кнопка для отображения операций
-        self.button_show_operations = tk.Button(self.root, text="Show Operations", font=button_font,
+        self.button_show_operations = tk.Button(self.root, text="Показать операции", font=button_font,
                                                 command=self.show_operations)
         self.button_show_operations.grid(row=1, column=4, padx=10, pady=10)
 
     def add_product_window(self):
         # Окно добавления продукта
         new_window = tk.Toplevel(self.root)
-        new_window.title("Add Product")
+        new_window.title("Добавить товар")
 
         label_font = ("Arial", 14)
 
-        tk.Label(new_window, text="Product Name", font=label_font).grid(row=0, column=0, padx=10, pady=10)
+        tk.Label(new_window, text="Название товара", font=label_font).grid(row=0, column=0, padx=10, pady=10)
         entry_product_name = tk.Entry(new_window)
         entry_product_name.grid(row=0, column=1, padx=10, pady=10)
 
-        tk.Label(new_window, text="Unit Price", font=label_font).grid(row=1, column=0, padx=10, pady=10)
+        tk.Label(new_window, text="Цена", font=label_font).grid(row=1, column=0, padx=10, pady=10)
         entry_unit_price = tk.Entry(new_window)
         entry_unit_price.grid(row=1, column=1, padx=10, pady=10)
 
-        tk.Label(new_window, text="Category", font=label_font).grid(row=2, column=0, padx=10, pady=10)
+        tk.Label(new_window, text="Категория", font=label_font).grid(row=2, column=0, padx=10, pady=10)
         entry_category = tk.Entry(new_window)
         entry_category.grid(row=2, column=1, padx=10, pady=10)
 
-        tk.Button(new_window, text="Add", command=lambda: self.add_product(entry_product_name, entry_unit_price, entry_category, new_window)).grid(row=3, column=0, columnspan=2, pady=10)
+        tk.Button(new_window, text="Добавить", command=lambda: self.add_product(entry_product_name, entry_unit_price, entry_category, new_window)).grid(row=3, column=0, columnspan=2, pady=10)
 
     def add_product(self, entry_product_name, entry_unit_price, entry_category, window):
         product_name = entry_product_name.get()
@@ -136,14 +119,13 @@ class InventoryApp:
 
         if product_name and unit_price and category:
             self.product.add_product(product_name, unit_price, category)
-            messagebox.showinfo("Success", "Product added successfully!")
+            messagebox.showinfo("Успех", "Товар успешно добавлен!")
             window.destroy()
             self.show_products()
         else:
-            messagebox.showwarning("Input Error", "Please fill all fields")
+            messagebox.showwarning("Ошибка ввода", "Пожалуйста заполните все поля")
 
     def clear_table(self):
-        # Очистить таблицу
         for i in self.tree.get_children():
             self.tree.delete(i)
 
@@ -151,30 +133,29 @@ class InventoryApp:
         # Настроить столбцы для продуктов
         self.tree["columns"] = ("ProductID", "ProductName", "UnitPrice", "Category")
         self.tree.heading("ProductID", text="ID")
-        self.tree.heading("ProductName", text="Product Name")
-        self.tree.heading("UnitPrice", text="Unit Price")
-        self.tree.heading("Category", text="Category")
+        self.tree.heading("ProductName", text="Название товара")
+        self.tree.heading("UnitPrice", text="Цена")
+        self.tree.heading("Category", text="Категория")
 
     def configure_tree_for_houses(self):
         # Настроить столбцы для складов
         self.tree["columns"] = ("HouseID", "Name", "Location")
         self.tree.heading("HouseID", text="ID")
-        self.tree.heading("Name", text="Name")
-        self.tree.heading("Location", text="Location")
+        self.tree.heading("Name", text="Название")
+        self.tree.heading("Location", text="Локация")
 
     def configure_tree_for_operations(self):
         # Настроить столбцы для операций
         self.tree["columns"] = ("OperationID", "ProductID", "HouseID", "TypeOperation", "Kol")
         self.tree.heading("OperationID", text="ID")
-        self.tree.heading("ProductID", text="Product ID")
-        self.tree.heading("HouseID", text="House ID")
-        self.tree.heading("TypeOperation", text="Type")
-        self.tree.heading("Kol", text="Quantity")
+        self.tree.heading("ProductID", text="ID Товара")
+        self.tree.heading("HouseID", text="ID Склада")
+        self.tree.heading("TypeOperation", text="Тип")
+        self.tree.heading("Kol", text="Количество")
 
     def show_products(self):
         self.clear_table()
         self.configure_tree_for_products()
-        # Заполнить таблицу продуктами
         products = self.product.get_all_products()
         for product in products:
             self.tree.insert("", "end", values=product)
@@ -184,15 +165,14 @@ class InventoryApp:
         if selected_item:
             product_id = self.tree.item(selected_item)["values"][0]
             self.product.delete_product(product_id)
-            messagebox.showinfo("Success", "Product deleted successfully!")
+            messagebox.showinfo("Успех", "Товар успешно удален!")
             self.show_products()
         else:
-            messagebox.showwarning("Selection Error", "Please select a product to delete")
+            messagebox.showwarning("Ошибка выбора", "Пожалуйста выделите товар для удаления")
 
     def show_houses(self):
         self.clear_table()
         self.configure_tree_for_houses()
-        # Заполнить таблицу складами
         houses = self.house.get_all_houses()
         for house in houses:
             self.tree.insert("", "end", values=house)
@@ -200,7 +180,6 @@ class InventoryApp:
     def show_operations(self):
         self.clear_table()
         self.configure_tree_for_operations()
-        # Заполнить таблицу операциями
         operations = self.operation.get_all_operations()
         for operation in operations:
             self.tree.insert("", "end", values=operation)
@@ -208,10 +187,8 @@ class InventoryApp:
 
 # Основной запуск программы
 if __name__ == "__main__":
-    db = Database('inventorySystem.db')  # Указываем название вашей базы данных
+    db = Database('inventorySystem.db')
     root = tk.Tk()
-
-    # Установка окна в полноэкранный режим
     root.state('zoomed')
 
     app = InventoryApp(root, db)
